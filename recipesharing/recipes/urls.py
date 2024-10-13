b43642e4d4227325ac4e060f3bc8647731e27a4b
m75-ali <116349197+m75-ali@users.noreply.github.com>
2024-10-13 11:16:44 +0100

    Recipe CRUD logic added. Basic bootstrap styles implemented

--- recipesharing/recipes/urls.py
+++ recipesharing/recipes/urls.py
@@ -2,9 +2,11 @@ from django.urls import path
 from . import views  # Import views from the current directory
 
 urlpatterns = [
-    path('', views.index, name='index'),  # Add a basic route
-    path('<int:recipe_id>/', views.recipe_detail, name='recipe_detail'),  # Recipe detail page
+    path('', views.index, name='recipe_index'),  # Use index for the main recipe page
+    path('<int:recipe_id>/', views.recipe_detail, name='recipe_detail'),
     path('add/', views.add_recipe, name='add_recipe'),  # Add recipe page
+    path('<int:recipe_id>/edit/', views.edit_recipe, name='edit_recipe'),
+    path('<int:recipe_id>/delete/', views.delete_recipe, name='delete_recipe'),
 
 
 ]
--- recipesharing/recipes/views.py
+++ recipesharing/recipes/views.py
@@ -1,13 +1,8 @@
-from django.shortcuts import render, get_object_or_404
-from django.shortcuts import redirect
+from django.shortcuts import render, get_object_or_404, redirect
 from .forms import RecipeForm
 from .models import Recipe
 
 # Create your views here.
-def index(request):
-    return render(request, 'recipes/index.html')
-
-
 def index(request):
     recipes = Recipe.objects.all()  # Fetch all recipes from the database
     return render(request, 'recipes/index.html', {'recipes': recipes})
@@ -17,11 +12,29 @@ def add_recipe(request):
         form = RecipeForm(request.POST)
         if form.is_valid():
             form.save()
-            return redirect('recipe_index')
+            return redirect('recipe_index')  # Redirect to recipe index
     else:
         form = RecipeForm()
     return render(request, 'recipes/add_recipe.html', {'form': form})
 
-def recipe_detail(request, id):
-    recipe = get_object_or_404(Recipe, id=id)  # Get the recipe by its ID
+def recipe_detail(request, recipe_id):  # Change 'id' to 'recipe_id'
+    recipe = get_object_or_404(Recipe, id=recipe_id)  # Fetch recipe using the 'recipe_id'
     return render(request, 'recipes/recipe_detail.html', {'recipe': recipe})
+
+def edit_recipe(request, recipe_id):
+    recipe = get_object_or_404(Recipe, id=recipe_id)
+    if request.method == 'POST':
+        form = RecipeForm(request.POST, instance=recipe)
+        if form.is_valid():
+            form.save()
+            return redirect('recipe_detail', recipe_id=recipe.id)
+    else:
+        form = RecipeForm(instance=recipe)
+    return render(request, 'recipes/edit_recipe.html', {'form': form, 'recipe': recipe})
+
+def delete_recipe(request, recipe_id):
+    recipe = get_object_or_404(Recipe, id=recipe_id)
+    if request.method == 'POST':
+        recipe.delete()
+        return redirect('recipe_index')
+    return render(request, 'recipes/delete_recipe.html', {'recipe': recipe})
