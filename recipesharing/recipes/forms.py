fd9d42f8e774319094e50249a6905407a0fc3a6f
m75-ali <116349197+m75-ali@users.noreply.github.com>
2024-10-21 22:08:48 +0100

    Styles updated

--- recipesharing/recipes/forms.py
+++ recipesharing/recipes/forms.py
@@ -5,3 +5,9 @@ class RecipeForm(forms.ModelForm):
     class Meta:
         model = Recipe
         fields = ['title', 'description', 'ingredients', 'instructions', 'category']
+
+    def __init__(self, *args, **kwargs):
+        super(RecipeForm, self).__init__(*args, **kwargs)
+        # Add Bootstrap classes to each field
+        for field_name, field in self.fields.items():
+            field.widget.attrs['class'] = 'form-control'
