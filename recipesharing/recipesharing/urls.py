7657c85c9ab66a57c62fac7dea6cb9ee12ba164b
m75-ali <116349197+m75-ali@users.noreply.github.com>
2025-04-15 03:46:12 +0100

    URL fixed for deployment

--- recipesharing/recipesharing/urls.py
+++ recipesharing/recipesharing/urls.py
@@ -5,13 +5,13 @@ from django.views.static import serve
 from django.conf import settings
 from django.conf.urls.static import static
 from django.shortcuts import redirect
-from recipes.views import recipe_index  # Import the actual view
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('recipes/', include('recipes.urls')),
-    path('users/', include('users.urls')),
-    path('', recipe_index, name='home'),  # Use the actual view function
+    path('recipes/', include('recipes.urls')),  # Recipe-related URLs
+    path('users/', include('users.urls')),  # User-related URLs
+    # Modify this to redirect to the recipes/ URL pattern instead of a named URL
+    path("", lambda request: redirect("recipes/")), 
     re_path(r'^media/(?P<path>.*)$', serve, {'document_root': settings.MEDIA_ROOT}),
 ]
 
